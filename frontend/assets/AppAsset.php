afab1052f8db77842e6768e43adbf47d0df9c5ea
hisashi161 <temirboyeb0823@gmail.com>
2021-09-02 14:59:14 +0500

    added patient and doctor pages

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -20,6 +20,8 @@ class AppAsset extends AssetBundle
         'css/elite/jquery.toast.css',
         'css/elite/style.min.css',
         'css/elite/dashboard1.css',
+        'css/elite/dataTables.bootstrap4.css',
+        'css/elite/responsive.dataTables.min.css',
     ];
     public $js = [
         'js/elite/waves.js',
@@ -33,6 +35,16 @@ class AppAsset extends AssetBundle
         'js/elite/raphael-min.js',
         'js/elite/morris.min.js',
         'js/elite/jquery.sparkline.min.js',
+        // 'js/elite/sticky-kit.min.js',
+        // 'js/elite/jquery.dataTables.min.js',
+        // 'js/elite/dataTables.responsive.min.js',
+        // 'js/elite/buttons.flash.min.js',
+        // 'js/elite/buttons.html5.min.js',
+        // 'js/elite/buttons.print.min.js',
+        // 'js/elite/dataTables.buttons.min.js',
+        // 'js/elite/jszip.min.js',
+        // 'js/elite/pdfmake.min.js',
+        // 'js/elite/vfs_fonts.js',
         // 'js/elite/jquery.toast.js',
         'js/clock.js',
     ];
